cebab8c58f1551f76e55034a186266759d9aaf8a
Lvin <977929611@qq.com>
2024-09-26 16:11:14 +0800

    更新lvgl为9.1.0，SquareLine Smart_gadget可以跑起来了，背景图片颜色也正常

--- main/gui_app/components/ui_comp_small_label.h
+++ main/gui_app/components/ui_comp_small_label.h
@@ -3,12 +3,19 @@
 // LVGL version: 9.1.0
 // Project name: Smart_Gadget
 
-#ifndef _UI_EVENTS_H
-#define _UI_EVENTS_H
+#ifndef _UI_COMP_SMALL_LABEL_H
+#define _UI_COMP_SMALL_LABEL_H
+
+#include "../ui.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+// COMPONENT Small Label
+#define UI_COMP_SMALL_LABEL_SMALL_LABEL 0
+#define _UI_COMP_SMALL_LABEL_NUM 1
+lv_obj_t * ui_Small_Label_create(lv_obj_t * comp_parent);
 
 #ifdef __cplusplus
 } /*extern "C"*/
